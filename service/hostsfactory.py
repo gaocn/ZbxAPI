2a8881b9d7dabdbddfa78cb7f68d14f155a3dfe7
高文文 <gaowenwencn@163.com>
2017-11-08 16:03:51 +0800

    根据监控主机所在的group_name，判断监控主机的类型（es,ngx, collector, indexer,kafka），根据监控主机类型自动启用相应服务，同时关闭其他服务。监控模板默认开始所有监控项

--- service/hostsfactory.py
+++ service/hostsfactory.py
@@ -76,6 +76,50 @@ class HostsFactory(object):
         else:
             raise E3CZbxException('there is no template named "%s"' % self.__template)
 
+    def get_item_id(self, key, hostid):
+        params = {
+            "filter": {
+                "hostid": hostid,
+                "key_": "custom.ulog.service.stat[%s]" % key
+            }
+        }
+        try:
+            result = self.__zapi.item.get(params)
+            return result[0]['itemid']
+        except E3CZbxException:
+            return -1
+
+    def disable_service(self, hostid):
+        """
+        根据组名关闭，监控主机中不关心的监控项
+        默认连接到模板中的监控项是全部启用的，但是对于模板中的有些监控项可能是不需要的。
+            例如：　当前主机是运行ES服务的，那么其他(如：nginx, indexer, collector, gateway)监控项是不需要开启的
+        因此，这里的处理方法是根据group_name判断当前添加的主机的类型，需要注意的是group_name中要有关键字.
+        [约定] 如下：
+             1. 对于ES服务其中的主机，其group_name中要包含关键字 "es" （不区分大小写），这样默认只会enable es相关的监控项；
+             2. 对于nginx服务其中的主机，其group_name中要包含关键字 "nginx" （不区分大小写），这样默认只会enable nginx相关的监控项；
+             3. 对于indexer服务其中的主机，其group_name中要包含关键字 "indexer" （不区分大小写），这样默认只会enable indexer相关的监控项；
+             4. 对于collector服务其中的主机，其group_name中要包含关键字 "collector" （不区分大小写），这样默认只会enable collector相关的监控项；
+             5. 对于gateway服务其中的主机，其group_name中要包含关键字 "gateway" （不区分大小写），这样默认只会enable gateway相关的监控项；
+        :param group_name:
+        :param hostid:
+        :return:
+        """
+        item_keys = ['kafka', 'es', 'collector', 'indexer', 'ngx']
+        itemids = []
+
+        for key in item_keys:
+            if key in self.__group.lower():
+                continue
+            itemid = self.get_item_id(key, hostid)
+            if itemid != -1:
+                itemids.append(itemid)
+
+        params = {"status": 1}
+        for itemid in itemids:
+            params['itemid'] = itemid
+            self.__zapi.item.update(params)
+
     def __create_host_helper(self, host, templateid, groupid):
         params = {
             "host": host,
@@ -105,6 +149,9 @@ class HostsFactory(object):
         try:
             result = self.__zapi.host.create(params)
             print('[SUCCESS] create host: %s (hostid=%s) and link to template: %s' % (host, result['hostids'], self.__template))
+
+            # 根据group_name将非相关的UlogService中的监控项禁用
+            self.disable_service(result['hostids'])
         except E3CZbxException as e:
             if 'exists' in e.__str__():
                 print('[SUCCESS] existed host: %s' % (host))
@@ -133,13 +180,14 @@ class HostsFactory(object):
         for host in self.__hosts:
             self.__create_host_helper(host, templateid, groupid)
 
+
 if __name__ == "__main__":
     pass
     # NginxServer = ['10.233.87.54']
     # template_name = 'ulog_system_stats_template_zbx20'
     #
     # host = HostsFactory('http://10.233.87.54:9090', 'admin', 'zabbix', template_name,
-    #                     'Nginx Servers', ['10.233.87.54'])
+    #                     'ESServers', ['10.230.135.126'])
     # host.create_host_link_template()
     #
     # EsServers = ['10.230.135.126', '10.230.135.127', '10.230.135.128']
@@ -151,3 +199,4 @@ if __name__ == "__main__":
     # host.create_host_link_template(hosts=CollectorServers, group_name="Collector Servers")
     # host.create_host_link_template(hosts=IndexerServers, group_name="Indexer Servers")
     # host.create_host_link_template(hosts=KafkaServers, group_name="Kafka Servers")
+    # host.disable_service('10087')
